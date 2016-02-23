0b36cd04da95fa4f9548aa158888cc7459149727
Maximilian Speicher <max@bitstars.com>
2016-02-23 22:39:43 +0100

    got rid of window.setInterval

--- sparkle.jquery.js
+++ sparkle.jquery.js
@@ -25,9 +25,11 @@
       fill: "#fff",
       stroke: "#000",
       size: 20,
+      delay: 0,
       duration: 1500,
       pause: 1000
     }, options);
+    var cssAnimationAttr = "my-sparkle " + settings.duration + "ms infinite linear";
 
     var $star = $('<svg class="my-sparkle" version="1.1" viewBox="0.0 0.0 50.0 50.0" fill="none" stroke="none" stroke-linecap="square" stroke-miterlimit="10" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><clipPath id="p.0"><path d="m0 0l50.0 0l0 50.0l-50.0 0l0 -50.0z" clip-rule="nonzero"></path></clipPath><g clip-path="url(#p.0)"><path fill="' + settings.stroke + '" fill-opacity="0.0" d="m0 0l50.0 0l0 50.0l-50.0 0z" fill-rule="nonzero"></path><path fill="' + settings.fill + '" d="m0.62204725 25.0l20.068499 -4.323374l4.309454 -20.13332l4.309454 20.13332l20.068499 4.323374l-20.068499 4.323374l-4.309454 20.133318l-4.309454 -20.133318z" fill-rule="nonzero"></path><path stroke="' + settings.stroke + '" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt" d="m0.62204725 25.0l20.068499 -4.323374l4.309454 -20.13332l4.309454 20.13332l20.068499 4.323374l-20.068499 4.323374l-4.309454 20.133318l-4.309454 -20.133318z" fill-rule="nonzero"></path></g></svg>').css({
         position: "absolute",
@@ -46,23 +48,33 @@
       };
     };
 
-    var placeStar = function() {
+    var placeStar = function(init) {
       var coords = getCoordinates();
 
-      $this.append(
-        $star.css({
-          "-moz-animation": "my-sparkle " + settings.duration + "ms infinite linear",
-          "-webkit-animation": "my-sparkle " + settings.duration + "ms infinite linear",
-          animation: "my-sparkle " + settings.duration + "ms infinite linear",
-          left: coords.left,
-          top: coords.top
-        }).show()
-      );
+      if (init) {
+        $this.append($star);
+      }
+
+      $star.css({
+        "-moz-animation": cssAnimationAttr,
+        "-webkit-animation": cssAnimationAttr,
+        animation: cssAnimationAttr,
+        display: "block",
+        left: coords.left,
+        top: coords.top
+      });
 
       window.setTimeout(function() {
-        $star.hide().css({
-          animation: null
+        $star.css({
+          "-moz-animation": null,
+          "-webkit-animation": null,
+          animation: null,
+          display: "none"
         });
+        
+        window.setTimeout(function() {
+          placeStar(false);
+        }, settings.pause);
       }, settings.duration);
     };
 
@@ -71,9 +83,9 @@
     }
 
     window.setTimeout(function() {
-      $.sparkleInterval[id] = window.setInterval(placeStar, settings.duration + settings.pause);
+      placeStar(true);
     }, settings.delay);
-
+    
     $.merge(ids, [id]);
     this.data("sparkle-ids", ids);
 
