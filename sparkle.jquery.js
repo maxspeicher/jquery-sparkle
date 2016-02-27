bd487f6e0335d62a06560117e58390198bfbf66d
Maximilian Speicher <max@bitstars.com>
2016-02-27 20:33:38 +0100

    perform destroy only if SVGs are present

--- sparkle.jquery.js
+++ sparkle.jquery.js
@@ -4,7 +4,7 @@
     $.destroySparkle = $.destroySparkle || {};
     var id = this.data("sparkle-id") || (new Date()).getTime() + Math.random();
     
-    if (options === "destroy") {
+    if (options === "destroy" && this.find("svg").length > 0) {
       $.destroySparkle[id] = true;
       this.data("sparkle-id", null);
     }
